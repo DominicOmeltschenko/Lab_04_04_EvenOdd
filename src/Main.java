ddbb3743e95756a2c6dfb38e81c1b9e79e036797
Dominic <Domdombossy@gmail.com>
2024-09-19 14:34:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int numberToTest = 7;
+
+        if (numberToTest % 2 == 0) {
+            System.out.println(numberToTest + " is even");
+        }
+        else {
+            System.out.println(numberToTest + " is odd");
+        }
+
+    }
+}
